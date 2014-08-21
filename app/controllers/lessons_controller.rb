b0b6aabe6548ab5f27674bb2cc3f9b84bc38672a
Mark Rubenstein & Nick Bernard <student@epicodus.com>
2014-08-21 11:28:24 -0700

    add admin view to update lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,4 +3,43 @@ class LessonsController < ApplicationController
     @lessons = Lesson.all
     render('lessons/index.html.erb')
   end
+
+  def index_admin
+    @lessons = Lesson.all
+    render('lessons/index-admin.html.erb')
+  end
+
+  def new_admin
+    @lesson = Lesson.new
+    render('lessons/new-admin.html.erb')
+  end
+
+  def create_admin
+    @lesson = Lesson.new(params[:lesson])
+    if @lesson.save
+      redirect_to("/admin")
+    else
+      render('lessons/show-admin.html.erb')
+    end
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+    render('lessons/show.html.erb')
+  end
+
+  def show_admin
+    @lesson = Lesson.find(params[:id])
+    render('lessons/show-admin.html.erb')
+  end
+
+  def update_admin
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(params[:lesson])
+      flash[:notice] = "Your lesson was updated."
+      redirect_to("/admin/lessons/#{@lesson.id}")
+    else
+      render('lessons/show-admin.html.erb')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 HowToRaceBikes::Application.routes.draw do
   match('/', {via: :get, to: 'lessons#index'})
+  match('/lessons/:id', {via: :get, to: 'lessons#show'})
+  match('/admin/lessons/:id', {via: [:patch, :put], to: 'lessons#update_admin'})
+
+  match('/admin', {via: :get, to: 'lessons#index_admin'})
+  match('/admin/lessons', {via: :post, to: 'lessons#create_admin'})
+  match('/admin/lessons/new', {via: :get, to: 'lessons#new_admin'})
+  match('/admin/lessons/:id', {via: :get, to: 'lessons#show_admin'})
+
+  # match('/lessons/:id', {via: :get, to: 'lessons#show'})
 end
